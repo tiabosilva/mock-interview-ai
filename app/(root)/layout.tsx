f0fa1de2ec795417698abee84ac463b601a56cdb
tiabosilva <tiabo.silva.ronaldo@gmail.com>
2025-04-02 17:16:34 +0100

    setup app, file and folder structure ,routing and auth pages

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,11 @@
+import React, { ReactNode } from 'react'
+
+const Rootlayout = ({children}: {children :ReactNode}) => {
+  return (
+    <div>
+      {children}
+    </div>
+  )
+}
+
+export default Rootlayout;
